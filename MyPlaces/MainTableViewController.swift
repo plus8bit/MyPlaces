12d038a0552b23f4f15a03a82b858cd645f30451
Nick <plus0bit@gmail.com>
2019-05-14 22:56:03 +0300

    Initial Commit

---
+++ MyPlaces/MainTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainTableViewController.swift
+//  MyPlaces
+//
+//  Created by Nick on 14/05/2019.
+//  Copyright © 2019 Nick Danilov. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+    
+    let restauranNames = [ "Dandelyan", "American Bar", "Manhattan", "The NoMad",
+                           "Connaught Bar", "Bar Termini", "The Clumsies", "Atlas" ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return restauranNames.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = restauranNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restauranNames[indexPath.row])
+        
+        return cell
+    }
+
+}
