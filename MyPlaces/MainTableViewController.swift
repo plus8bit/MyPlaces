39a693de5ac630fc41fda991a7ac89f20a44f9b0
Nick <plus0bit@gmail.com>
2019-05-19 13:49:34 +0300

    Create NewPlaceViewController & add design for it

--- MyPlaces/MainTableViewController.swift
+++ MyPlaces/MainTableViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class MainTableViewController: UITableViewController {
     
-    let restauranNames = [ "Dandelyan", "American Bar", "Manhattan", "The NoMad",
-                           "Connaught Bar", "Bar Termini", "The Clumsies", "Atlas" ]
+    var myPlaces = Place.getPlaces()
+//        Place(name: "Dandelyan", location: "Montreal", type: "Restaurant", image: "Dandelyan.jpg")
+//        Place(name: "American Bar", location: "New York", type: "Restaurant", image: "American Bar.jpg"),
+//        Place(name: "The NoMad", location: "Milan", type: "Bar", image: "The NoMad.jpg"),
+//        Place(name: "Connaught Bar", location: "London", type: "Bar", image: "Connaught Bar.jpg"),
+//        Place(name: "Bar Termini", location: "San Francisco", type: "Restaurant", image: "Bar Termini.jpg"),
+//        Place(name: "The Clumsies", location: "Paris", type: "Restaurant", image: "The Clumsies.jpg"),
+//        Place(name: "Atlas", location: "Tokyo", type: "Restaurant", image: "Dandelyan.jpg"),
+//        Place(name: "Manhattan", location: "New York", type: "Restaurant", image: "Manhattan.jpg"),
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,14 +29,18 @@ class MainTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return restauranNames.count
+        return myPlaces.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
-        cell.nameLabel?.text = restauranNames[indexPath.row]
-        cell.imageOfPlace?.image = UIImage(named: restauranNames[indexPath.row])
+        cell.nameLabel?.text = myPlaces[indexPath.row].name
+        cell.imageOfPlace?.image = UIImage(named: myPlaces[indexPath.row].image)
+        cell.locationLabel.text = myPlaces[indexPath.row].location
+        cell.typeLabel.text = myPlaces[indexPath.row].type
+        
+        
         cell.imageOfPlace?.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
         cell.imageOfPlace?.clipsToBounds = true
         
@@ -39,5 +51,9 @@ class MainTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 85
     }
+    
+    @IBAction func cancelAction(_ sender: UIStoryboardSegue) {}
+    
+    
 
 }
